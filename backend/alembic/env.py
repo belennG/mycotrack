a81910295cecd61d1ed5085d7969c70b89558bba
Maria Belen Gatti <mariabelengatti@Marias-MacBook-Pro.local>
2026-08-02 19:01:54 +0100

    fix alembic env to use db url

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from database import Base
+from database import Base, SQLALCHEMY_DATABASE_URL
 from models.batch import Batch  # noqa
 from models.daily_log import DailyLog  # noqa
 from models.alert import Alert  # noqa
@@ -13,7 +13,8 @@ from models.alert import Alert  # noqa
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+if SQLALCHEMY_DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
