eec34bac7602239d72d649b3c637ffdb53c31ff6
Maria Belen Gatti <mariabelengatti@Marias-MacBook-Pro.local>
2026-08-02 18:36:20 +0100

    fixbuilding of db string

--- backend/database.py
+++ backend/database.py
@@ -6,16 +6,19 @@ from dotenv import load_dotenv
 # Load variables from the .env file
 load_dotenv()
 
-# Build the connection string
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+if not SQLALCHEMY_DATABASE_URL:
+    # Build the connection string
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME")
+
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
 
 # Core interface to the db
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
