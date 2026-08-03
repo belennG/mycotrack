469b6393446b8576b8254ffc34e085b892218b17
Maria Belen Gatti <mariabelengatti@Marias-MacBook-Pro.local>
2026-08-03 16:20:50 +0100

    34 Install Chakra UI, wrap in provider, add chakra ai skills

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import { system } from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ChakraProvider value={system}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </ChakraProvider>
 )
