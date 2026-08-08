b27616397a2c0a0980fe6485729ccce2846400f9
Maria Belen Gatti <mariabelengatti@Marias-MacBook-Pro.local>
2026-08-08 19:16:45 +0100

    Add deployed fe to allowed origin cors middleware

--- backend/middleware/cors.py
+++ backend/middleware/cors.py
@@ -9,7 +9,10 @@ def setup_cors(app: FastAPI) -> None:
     """
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173"],
+        allow_origins=[
+            "http://localhost:5173",
+            "http://mycotrack-frontend-prod-2026.s3-website.eu-north-1.amazonaws.com",
+        ],
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allow_headers=["Content-Type", "Authorization"],
